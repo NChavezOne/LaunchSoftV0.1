b8cd3f18b32d0e4ed4a7489553c21aa7aaf2245b
Nicholas <NChavez0516@gmail.com>
2025-05-12 17:21:12 -0700

    Latest commit

--- src/Launchsoft_v0.1.cpp
+++ src/Launchsoft_v0.1.cpp
@@ -15,7 +15,11 @@
 //local pin defines
 
 #define BUTTON_PIN 2
+
 #define BUZZER_PIN 3
+#define BUZZER_PIN_2 4
+#define BUZZER_PIN_3 5 //buzzer 3 is a passive buzzer that requires a logic low to sound
+
 #define RELAY_PIN 4
 
 // These are the Arduino pins required to create a software seiral
@@ -25,7 +29,7 @@ const int softwareRx = 7;
 
 SoftwareSerial s7s(softwareRx, softwareTx);
 
-StartupSound buzzer(BUZZER_PIN,BUZZER_PIN,BUZZER_PIN);
+StartupSound buzzer(BUZZER_PIN, BUZZER_PIN_2 ,BUZZER_PIN_3);
 
 //function defines
 
@@ -35,7 +39,6 @@ void clearDisplay()
 {
   s7s.write(0x76);  // Clear display command
 }
-
 // Set the displays brightness. Should receive byte with the value
 //  to set the brightness to
 //  dimmest------------->brightest
@@ -45,7 +48,6 @@ void setBrightness(byte value)
   s7s.write(0x7A);  // Set brightness command byte
   s7s.write(value);  // brightness data byte
 }
-
 // Turn on any, none, or all of the decimals.
 //  The six lowest bits in the decimals parameter sets a decimal 
 //  (or colon, or apostrophe) on or off. A 1 indicates on, 0 off.
@@ -56,6 +58,7 @@ void setDecimals(byte decimals)
   s7s.write(decimals);
 }
 
+//personal functions
 void neutralDisplay() 
 {
   //send cursor to furthest left position
@@ -66,14 +69,23 @@ void neutralDisplay()
     s7s.write(0b00101101); //central segment
   }
 }
+void soundBuzzer3() {
+  tone(BUZZER_PIN_3, 1, 500);
+}
 
 //global variables
 
-int i = 30;
+int countdown = 30;
 
 //setup
 
 void setup() {
+  //pinmodes
+  
+  pinMode(BUZZER_PIN_3, OUTPUT);
+  digitalWrite(BUZZER_PIN_3, HIGH);
+  
+  
   s7s.begin(9600);
   Serial.begin(9600);
 
@@ -83,6 +95,22 @@ void setup() {
 
   neutralDisplay();
   buzzer.tune1();
+  
+  Serial.print("Device initializing.");
+  for (int i =0; i < 5; i++) {
+    Serial.print(".");
+    delay(50);
+  }
+  Serial.println(".");
+  
+  bool allDevicesInitialized = true;
+  if (allDevicesInitialized == true) {
+    Serial.println("Device successfully initialized.");
+  } else {
+    buzzer.tune4();
+    Serial.println("Device failed to initialized.");
+  }
+
   delay(1000);
   buzzer.tune3();
   delay(1000);
@@ -97,20 +125,26 @@ void setup() {
 
 //loop
 
+long int timeElapsedSinceLastLoop = 0;
+
 void loop() {
   // put your main code here, to run repeatedly:
   if (digitalRead(BUTTON_PIN) == LOW) {
-    while (i >= 0) {
+    Serial.println("Countdown buzzon pressed, launch sequence commencing.");
+    delay(100);
+    while (countdown >= 0) {
+      timeElapsedSinceLastLoop = millis();
       clearDisplay();
-      s7s.print(i);
+      s7s.print(countdown);
       tone(BUZZER_PIN, 2000, 200);
-      delay(800);
-      if (i == 0) {
+      soundBuzzer3();
+      if (countdown == 0) {
         digitalWrite(RELAY_PIN, HIGH);
         tone(BUZZER_PIN, 2000);
         while(1) {}
       }
-      i--;
-    }
+      countdown--;
+      while (millis() - timeElapsedSinceLastLoop <= 1000) {}
+    } 
   }
 }
